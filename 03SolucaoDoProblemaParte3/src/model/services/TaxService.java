83d6b439fea7315f9c7b391cdb744bf8a34956c7
Leonardo Santos <leonardojosantos@gmail.com>
2023-12-09 22:53:39 -0300

    Problem solution - part 3

---
+++ 03SolucaoDoProblemaParte3/src/model/services/TaxService.java
@@ -0,0 +1,9 @@
+/* -------------------- packages section -------------------- */
+package model.services;
+
+/* -------------------- TaxService interface -------------------- */
+public interface TaxService {
+
+	/* -------------------- methods section -------------------- */
+	double tax(double amount);
+}
